1fd527dee8a87c9d5d4fb40ab2ec3d85193a5751
daphneruth <daphneruth304@gmail.com>
2022-12-21 21:07:27 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -14,7 +14,9 @@ function App() {
           <div  className='products'>
             {data.products.map(product =>(
             <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
             <img src={product.image} alt={product.name} />
+            </a>
             <div className="product-info">
             <p>{product.name}</p>
             <p>{product.price}</p>
