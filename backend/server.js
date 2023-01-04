f6aec39b0536347dd656763e7b6181ff04bbe986
daphneruth <daphneruth304@gmail.com>
2023-01-04 18:18:37 +0300

    running simple server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import data from '../data';
+import data from '../data.js';
 
 const app = express();
 
 app.get('/api/products', (req, res) => {
   res.send(data.products)
 
+})
+
+const port =process.env.PORT || 5000
+app.listen(port, (err, res) => {
+    console.log(`serve at http://localhost:${port}`)
 })
