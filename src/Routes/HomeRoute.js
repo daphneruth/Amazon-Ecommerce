ad14932bd38315528c151def00432cae574504a9
daphneruth <daphneruth304@gmail.com>
2023-01-04 14:03:52 +0300

    using Link to

--- src/Routes/HomeRoute.js
+++ src/Routes/HomeRoute.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import React from 'react'
 import data from '../data'
 
@@ -8,11 +9,15 @@ function HomeRoute() {
           <div  className='products'>
             {data.products.map(product =>(
             <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-            <img src={product.image} alt={product.name} />
-            </a>
+              <Link to={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+            </Link>
             <div className="product-info">
+
+            <Link to={`/product/${product.slug}`}>
             <p>{product.name}</p>
+              </Link>
+          
             <p>{product.price}</p>
             <button>Add to Cart</button>
             </div>
