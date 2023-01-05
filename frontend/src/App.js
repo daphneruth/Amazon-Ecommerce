810a6dd8ae899af52ab3e7f67ac7fdcaa6b357e4
daphneruth <daphneruth304@gmail.com>
2023-01-05 09:51:11 +0300

    react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeRoute from './Routes/HomeRoute';
 import ProductRoute from './Routes/ProductRoute';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
     <div>
       <header >
+      <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+            <Navbar.Brand>amazona</Navbar.Brand>
+              </Container>
+
         <Link to ="/"> Amazon </Link>
       </header>
       <main>
