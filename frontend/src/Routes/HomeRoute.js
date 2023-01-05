0447c26a6d1106131ae7304caad8fa52a9723475
daphneruth <daphneruth304@gmail.com>
2023-01-05 09:24:23 +0300

    updated

--- frontend/src/Routes/HomeRoute.js
+++ frontend/src/Routes/HomeRoute.js
@@ -47,21 +47,30 @@ function HomeRoute() {
     <div>
        <h1>Featured Products</h1>
           <div  className='products'>
-            {products.map(product =>(
-            <div className="product" key={product.slug}>
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product-info">
 
-            <Link to={`/product/${product.slug}`}>
-            <p>{product.name}</p>
-              </Link>
-          
-            <p>{product.price}</p>
-            <button>Add to Cart</button>
-            </div>
-          </div>))}
+            {loading? (
+              <div>Loading...</div>
+            ) : error ? (
+               <div>error</div>)
+              :(
+
+                products.map(product =>(
+                  <div className="product" key={product.slug}>
+                    <Link to={`/product/${product.slug}`}>
+                      <img src={product.image} alt={product.name} />
+                  </Link>
+                  <div className="product-info">
+      
+                  <Link to={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                    </Link>
+                
+                  <p>{product.price}</p>
+                  <button>Add to Cart</button>
+                  </div>
+                </div>))
+
+              )}
       
       </div>
     </div>
