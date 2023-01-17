a06fe99e1b58e7672c00a2d4e2712283883eb581
daphneruth <daphneruth304@gmail.com>
2023-01-17 11:48:25 +0300

    fixed some bug

--- frontend/src/Components/Product.js
+++ frontend/src/Components/Product.js
@@ -7,14 +7,14 @@ function Product (props) {
         <Link to={`/product/${product.slug}`}>
           <img src={product.image} alt={product.name} />
       </Link>
-      <card.Body>
+      <Card.Body>
 
       <Link to={`/product/${product.slug}`}>
       <Card.Title>{product.name}</Card.Title>
         </Link>
         <Card.Text>${product.price}</Card.Text>
         <Button>Add to Cart</Button>
-     </card.Body>
+     </Card.Body>
      </Card>
 
     )
