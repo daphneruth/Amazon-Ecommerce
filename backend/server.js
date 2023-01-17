5dba5ae0e42aef10fb0efbb0ca4c13638d8493db
daphneruth <daphneruth304@gmail.com>
2023-01-17 12:37:09 +0300

    fixed some bug

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ app.get('api/products/slug/:slug', (req, res) => {
   if (product) {
     res.send(product)
 } else {
- res.status(404).send(message:"Product not found")})
+ res.status(404).send(message:"Product not found")}
+});
 
 const port =process.env.PORT || 5000
 app.listen(port, (err, res) => {
