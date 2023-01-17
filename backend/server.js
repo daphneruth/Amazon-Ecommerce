7e0078018845287b0ba84a743623e8b5854bc76b
daphneruth <daphneruth304@gmail.com>
2023-01-17 12:35:49 +0300

    creating product details api

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,13 @@ app.get('/api/products', (req, res) => {
 
 })
 
+app.get('api/products/slug/:slug', (req, res) => {
+  const product =data.products.find((x) => x.slug === req.params.slug)
+  if (product) {
+    res.send(product)
+} else {
+ res.status(404).send(message:"Product not found")})
+
 const port =process.env.PORT || 5000
 app.listen(port, (err, res) => {
     console.log(`serve at http://localhost:${port}`)
