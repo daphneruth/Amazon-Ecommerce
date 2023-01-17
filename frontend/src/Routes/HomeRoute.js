f9cb1f362d7b244078fc0f11390c07820d8e3cfb
daphneruth <daphneruth304@gmail.com>
2023-01-17 11:34:47 +0300

    updated product component

--- frontend/src/Routes/HomeRoute.js
+++ frontend/src/Routes/HomeRoute.js
@@ -58,22 +58,7 @@ function HomeRoute() {
               <Row>
                 { products.map(product =>(
                   <Col  sm={6} md={4} lg={3}>
-                    <div className="product" key={product.slug}>
-                    <Link to={`/product/${product.slug}`}>
-                      <img src={product.image} alt={product.name} />
-                  </Link>
-                  <div className="product-info">
-      
-                  <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                    </Link>
-                    <p>
-                    <strong>${product.price}</strong>
-                    </p>
-                    <button>Add to cart</button>
-                    
-                    </div>
-                    </div>
+                   
                   </Col>
                 
                  ))}
