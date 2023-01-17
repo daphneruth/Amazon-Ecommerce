513354d159886ea76802b89ffd5d30b0882106f3
daphneruth <daphneruth304@gmail.com>
2023-01-17 11:20:19 +0300

    updated

--- frontend/src/Routes/HomeRoute.js
+++ frontend/src/Routes/HomeRoute.js
@@ -2,7 +2,8 @@ import {useReducer, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import {logger }from  "use-reducer-logger"
-
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 //creating reducer function
 
