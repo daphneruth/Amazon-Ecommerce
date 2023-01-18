6a133e83d864f89dda5301b50f16b3be26a10f26
daphneruth <daphneruth304@gmail.com>
2023-01-18 21:18:10 +0300

    fixed some bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,35 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import HomeScreen from './Screens/HomeScreen';
-import ProductScreen from './Screens/ProductScreen';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Navbar';
-import { LinkContainer } from 'react-router-bootstrap';                
-//import ProductScreen from '../../../mern-amazona/frontend/src/screens/ProductScreen';
-//import HomeScreen from '../../../mern-amazona/frontend/src/screens/HomeScreen';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
     <BrowserRouter>
-    <div  className='d-flex  flex-column site-container'>
-      <header >
-      <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
-        <LinkContainer to ="/">
-            <Navbar.Brand>amazona</Navbar.Brand>
-            </LinkContainer>
+      <div className="d-flex  flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
             </Container>
-        </Navbar>
-        
-      </header>
-      <main>
-        <Container  className="mt-3">
-        <Routes>
-          <Route path="/product/:slug" element={<ProductScreen/>} />
-          <Route path="/" element={<HomeScreen/>} />
-        </Routes>
-       
-        </Container>
-       
-      </main>
-      <footer>
-        <div className="text-center">
-          All rights reserved.
-        </div>
-      </footer>
-    
-    </div>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+            <Route path="/products/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved.</div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { useReducer,useState, useEffect } from "react";
+import { useReducer, useEffect } from "react";
 
 import axios from "axios";
 
@@ -7,53 +7,50 @@ import Col from "react-bootstrap/Col";
 import Product from "../Components/Product";
 import { Helmet } from 'react-helmet-async';
 
-//creating reducer function
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "FETCH_REQUEST":
+    case 'FETCH_REQUEST':
       return { ...state, loading: true };
-    case "FETCH_SUCCESS":
+    case 'FETCH_SUCCESS':
       return { ...state, products: action.payload, loading: false };
-    case "FETCH_FAIL":
+    case 'FETCH_FAIL':
       return { ...state, loading: false, error: action.payload };
     default:
       return state;
   }
-};
-
+}
 function HomeScreen() {
-  // const [products, setProducts] = useState([])
-
   const [{ loading, error, products }, dispatch] = useReducer((reducer), {
     products: [],
     loading: true,
-    error: "",
+    error: '',
   });
-
+  // const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      dispatch({ type: "FETCH_REQUEST" });
+      dispatch({ type: 'FETCH_REQUEST' });
       try {
-        const result = await axios.get("/api/products");
-        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({ type: "FETCH_FAIL", payload: err.message });
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
       }
+      // setProducts(result.data);
     };
     fetchData();
   }, []);
   return (
     <div>
-       <Helmet>
-                <title>Amazon</title>
-              </Helmet>
+      <Helmet>
+        <title>Amazon</title>
+      </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
           <div>Loading...</div>
         ) : error ? (
-          <div>error</div>
+          <div>{error}</div>
         ) : (
           <Row>
             {products.map((product) => (
