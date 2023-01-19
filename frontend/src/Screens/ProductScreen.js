88e540162c3b5617d4491848cbcb73c7157e6435
daphneruth <daphneruth304@gmail.com>
2023-01-19 12:40:46 +0300

    updated

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -9,6 +9,7 @@ import Button from "react-bootstrap/Button";
 import Rating from "../Components/Rating";
 import { Helmet } from 'react-helmet-async';
 import Card from 'react-bootstrap/Card';
+import { getError } from "../utils";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -40,7 +41,7 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
-        dispatch({ type: "FETCH_FAIL", payload: err.message });
+        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
       }
     };
     fetchData();
