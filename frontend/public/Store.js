aad2ba0d276185a5804191f2b9685cbcb0d88342
daphneruth <daphneruth304@gmail.com>
2023-01-19 12:58:09 +0300

    cart store

--- frontend/public/Store.js
+++ frontend/public/Store.js
@@ -1,5 +1,6 @@
 import { createContext, useReducer } from 'react';
-export const Store= createContext():
+ 
+export const Store = createContext();
 
 const initialState = {
     cart: {
@@ -9,8 +10,16 @@ const initialState = {
 function reducer(state, action) {
     switch (action.type) {
 
-        case 'CART_ADD_ITEM'
-        
+        case 'CART_ADD_ITEM':
+        return{
+            ...state,
+            cart:{
+                ...state.cart,
+                cartItems:[...state.cart.cartItems, action.payload ]
+            }
+        }
+        default:
+            return state;
     }
 }
 export function StoreProvider(props) {
