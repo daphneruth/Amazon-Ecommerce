9ae99a4da2df5a09731e18c4108078a97e7011b3
daphneruth <daphneruth304@gmail.com>
2023-01-21 02:20:26 +0300

    created product details api

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,15 @@ app.get('/api/products/slug/:slug', (req, res) => {
   }
 });
 
+app.get('/api/products/slug/_id', (req, res) => {
+
+  const product = data.products.find((x) => x.id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
 
 
  const port = process.env.PORT || 3000;
