18c793bc605959dd28b16688033c6c53d32f17cd
daphneruth <daphneruth304@gmail.com>
2023-01-23 08:15:14 +0300

    added backend routes

--- backend/server.js
+++ backend/server.js
@@ -17,27 +17,6 @@ mongoose
 
 const app = express();
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-
-app.get("/api/products/slug/:slug", (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
-
-app.get("/api/products/slug/_id", (req, res) => {
-  const product = data.products.find((x) => x.id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
