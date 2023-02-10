633caa95e8ecb18fd947bbf3997851ead7260a5f
daphneruth <daphneruth304@gmail.com>
2023-02-10 19:28:28 +0300

    updated

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -52,7 +52,7 @@ function reducer(state, action) {
         cart: {
           cartItems: [],
           shippingAddress: {},
-         // paymentMethod: '',
+          paymentMethod: '',
         },
       };
     case 'SAVE_SHIPPING_ADDRESS':
@@ -68,12 +68,4 @@ function reducer(state, action) {
         ...state,
         cart: { ...state.cart, paymentMethod: action.payload },
       };
-    default:
-      return state;
-  }
-}
-export function StoreProvider(props) {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const value = { state, dispatch };
-  return <Store.Provider value={value}>{props.children} </Store.Provider>;
-}
+    default
