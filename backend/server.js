1bab7f77cb75a7c60f5395f66bf52b12f2fdaaca
daphneruth <daphneruth304@gmail.com>
2023-02-25 21:06:35 +0300

    updated

--- backend/server.js
+++ backend/server.js
@@ -32,5 +32,5 @@ app.use((err, req, res, next) => {
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
 
- // console.log(`serve http://localhost:${port}`);
+  console.log(`serve http://localhost:${port}`);
 });
